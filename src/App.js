7f1b2f3ff28d14286c4d9a38bfb211e11fdd4be0
Johny Kuang <kuang.johny2@gmail.com>
2020-11-16 18:25:35 -0800

    began time slot selection page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import Login from './Login'
 import Card from './CardComponent/Card'
 import DatesPage from './DatesPage/DatesPage'
+import TimesPage from './TimesPage/TimesPage'
 
 function App() {
   return (
   <div>
-    <div className='centered'>
+    <div style={styles.divSizing}>
         {/* <Login/> */}
         {/* <Card/> */}
-        <DatesPage/>
+        {/* <DatesPage/> */}
+        <TimesPage/>
     </div>
   </div>
   );
 }
 
-const headerStyle = {
-  marginTop: '500px'
+const styles = {
+  divSizing: {
+    height: '90vh',
+    // width: '10vh'
+  }
 }
 
 export default App;
