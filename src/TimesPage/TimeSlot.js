41115e21c90927b5cd692404f8507d8382b5125b
Johny Kuang <kuang.johny2@gmail.com>
2020-11-18 18:38:14 -0800

    made a couple changes to times and dates pages;

--- src/TimesPage/TimeSlot.js
+++ src/TimesPage/TimeSlot.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class TimeSlot extends Component {
-    render() {
-        return (
-            <div style={styles.outsideContainer}>
-                <text style={{fontSize:'10px', fontWeight:'bold'}}>{this.props.time}</text>
-                <div style = {styles.rectangle}/>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div style={styles.outsideContainer}>
+        <div style={styles.timeLabel}>{this.props.time}</div>
+        <div style={styles.rectangle}></div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    outsideContainer:{
-        display: 'flex',
-        // width: '70vw'
-    },
-    rectangle: {
-        // height: '65px',
-        // width: '250px',
-        height: '8vh',
-        width: '300px',
-        // width: 'calc(100vh-50px)',
-        background: '#19C1B7',
-        opacity: '0.2'
-    }
-}
+  outsideContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  timeLabel: {
+    fontSize: "10px",
+    fontWeight: "bold",
+    flexBasis: "30px", //min width
+    //flexGrow: 1,
+  },
+  rectangle: {
+    flexGrow: 1,
+    height: "8vh",
+    background: "#19C1B7",
+    opacity: "0.2",
+    borderBottom: "2px solid #1D1D1D",
+    borderTop: "2px solid #1D1D1D",
+  },
+};
 
-export default TimeSlot
+export default TimeSlot;
--- src/TimesPage/TimesPage.js
+++ src/TimesPage/TimesPage.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react'
-import SectionBar from '../DatesPage/SectionBar'
-import TimeSlot from './TimeSlot'
+import React, { Component } from "react";
+import SectionBar from "../DatesPage/SectionBar";
+import TimeSlot from "./TimeSlot";
 
-
-let times = ['8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM', '3 PM', '4 PM',
-             '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM', '12 AM']
+let times = [
+  "8 AM",
+  "9 AM",
+  "10 AM",
+  "11 AM",
+  "12 PM",
+  "1 PM",
+  "2 PM",
+  "3 PM",
+  "4 PM",
+  "5 PM",
+  "6 PM",
+  "7 PM",
+  "8 PM",
+  "9 PM",
+  "10 PM",
+  "11 PM",
+  "12 AM",
+];
 
 export class TimesPage extends Component {
-    render() {
-        var slots = []
-        times.map( (item,index) => slots.push(<TimeSlot time = {item}/> ))
-        return (
-            <div>
-                <SectionBar/>
-                {slots}
-            </div>
-        )
-    }
+  render() {
+    var slots = [];
+    times.map((item, index) => slots.push(<TimeSlot time={item} />));
+    return (
+      <div style={{ paddingRight: "20px", paddingLeft: "20px" }}>
+        <SectionBar />
+        {slots}
+      </div>
+    );
+  }
 }
 
-export default TimesPage
+export default TimesPage;
